e2fac0df5346727ef7b62941ab73cac867296401
Kyle Lastimosa <glitchkyle16@gmail.com>
2024-02-18 23:49:07 -0500

    Complete db seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,49 @@
-import { PrismaClient, User } from '@prisma/client'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './.env.local' })
+
+import { PrismaClient } from '@prisma/client'
 import { faker } from '@faker-js/faker'
 
 const numUsers = 50
+const maxProductsPerUser = 50
 
 const prisma = new PrismaClient()
 
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
 async function main() {
   await prisma.user.deleteMany({})
-  await prisma.order.deleteMany({})
   await prisma.product.deleteMany({})
-
-  const users: User[] = []
+  await prisma.order.deleteMany({})
 
   for (let i = 0; i < numUsers; i++) {
-    const user = {
-      email: faker.internet.email(),
-      sub: faker.string.uuid(),
-      picture: faker.image.avatar()
+    const user = await prisma.user.create({
+      data: {
+        sub: faker.string.uuid(),
+        email: faker.internet.email(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        emailVerified: Math.random() < 0.5,
+        picture: faker.image.avatar()
+      }
+    })
+
+    for (; i < getRandomInt(0, maxProductsPerUser); i++) {
+      await prisma.product.create({
+        data: {
+          name: faker.commerce.productName(),
+          description: faker.commerce.productDescription(),
+          sellerId: user.id,
+          picture: faker.image.urlLoremFlickr(),
+          price: Number(faker.commerce.price()),
+          currency: faker.finance.currency().code
+        }
+      })
     }
-
-    users.push(user as never as User)
   }
-
-  await prisma.user.createMany({ data: users })
 }
 
 main()
